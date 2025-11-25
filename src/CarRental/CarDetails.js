5e3661573362ba7dd6d911be9cc6582b44571af2
Bhanu Keerthi <bhanukeerthi1212@gmail.com>
2025-11-25 22:59:14 +0530

    car details fix

--- src/CarRental/CarDetails.js
+++ src/CarRental/CarDetails.js
@@ -4,35 +4,61 @@ import { useParams } from 'react-router-dom'
 import SendEnquiry from './Admin/SendEnquiry'
 
 const CarDetails = () => {
-    const { id } = useParams()
-    const [car, setCar] = useState({})
+  const { id } = useParams()
+  const [car, setCar] = useState(null)
+  const [loading, setLoading] = useState(true)
 
-    useEffect(() => {
-        axios
-      axios.get(`https://priya-car-travels-api.onrender.com/cars/${id}`)
+  useEffect(() => {
+    axios
+      .get(`https://priya-car-travels-api.onrender.com/cars/${id}`)
+      .then((res) => {
+        setCar(res.data)
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err)
+        setLoading(false)
+      })
+  }, [id])
 
+  if (loading) {
+    return <h3 className="text-center p-5">Loading car details...</h3>
+  }
 
-            .then((res) => setCar(res.data))
-            .catch((err) => console.log(err))
-    }, [id])
+  if (!car) {
+    return <h3 className="text-center p-5 text-danger">Car not found</h3>
+  }
 
-    return (
-        <div className='container p-5'>
-            <div className='row'>
-                <div className='col-lg-8'>
-                    <h2>{car.carName}</h2>
-                    <p>{car.description}</p>
-                    <p className='btn btn-dark me-3'>Category: {car.category}</p>
-                    <p className='btn btn-dark me-3'>Model: {car.model}</p>
-                    <p className='btn btn-dark'>{car.pricePerDay}/- per day</p>
-                </div>
+  return (
+    <div className="container p-5">
+      <div className="row">
 
-                <div className='col-lg-4'>
-                    <SendEnquiry />
-                </div>
-            </div>
+        <div className="col-lg-8">
+          <img
+            src={car.image}
+            alt={car.name}
+            className="img-fluid mb-4 rounded shadow"
+          />
+
+          <h2>{car.name}</h2>
+          <p>{car.description}</p>
+
+          <p className="btn btn-dark me-3">
+            Model: {car.model || "N/A"}
+          </p>
+
+          <p className="btn btn-dark">
+            ₹ {car.pricePerDay} / day
+          </p>
         </div>
-    )
+
+        <div className="col-lg-4">
+          <SendEnquiry />
+        </div>
+
+      </div>
+    </div>
+  )
 }
 
 export default CarDetails
